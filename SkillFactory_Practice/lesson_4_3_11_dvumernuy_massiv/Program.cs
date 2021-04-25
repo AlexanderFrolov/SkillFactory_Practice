d8689f72e7d8befe3a20f03396839fdb0abb0c71
AlexanderFrolov <sovereignty.alex@gmail.com>
2021-04-25 09:53:21 +0300

    added solution 4.3.7(reverse), 4.3.11(двумерный массив)

---
+++ SkillFactory_Practice/lesson_4_3_11_dvumernuy_massiv/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая отображает этот же массив, но только перебор начинается
+//    по столбцам. То есть сначала отображаем все знания строк первого столбца, потом
+//    второго и далее. 
+
+using System;
+
+namespace lesson_4_3_11_dvumernuy_massiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
+
+            for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+            {   
+                for (int j = 0; j < array.GetUpperBound(0) + 1; j++)
+                {
+                    Console.Write(array[j, i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ SkillFactory_Practice/module4_Basic_C_Sharp/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace module4_Basic_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Цикл for");
+            //for (int i = 1; i < 5; i++)
+            //{
+            //    Console.WriteLine("Iteration {0}", i);
+            //    switch (Console.ReadLine())
+            //    {
+            //        case "red":
+            //            Console.BackgroundColor = ConsoleColor.Red;
+            //            Console.ForegroundColor = ConsoleColor.Black;
+            //            Console.WriteLine("Your color is red!");
+            //            break;
+            //        case "green":
+            //            Console.BackgroundColor = ConsoleColor.Green;
+            //            Console.ForegroundColor = ConsoleColor.Black;
+            //            Console.WriteLine("Your color is green!");
+            //            break;
+            //        case "cyan":
+            //            Console.BackgroundColor = ConsoleColor.Cyan;
+            //            Console.ForegroundColor = ConsoleColor.Black;
+            //            Console.WriteLine("Your color is cyan!");
+            //            break;
+            //        default:
+            //            Console.BackgroundColor = ConsoleColor.Yellow;
+            //            Console.ForegroundColor = ConsoleColor.Red;
+            //            Console.WriteLine("Your color is yellow!");
+            //            break;
+            //    }
+            //}
+
+            Console.WriteLine("Цикл while");
+            int k = 0;
+            while (k < 2)
+            {
+                Console.WriteLine(k);
+                Console.WriteLine("Напишите свой любимый цвет с маленькой буквы на английском языке");
+                var text = Console.ReadLine();
+
+                if (text == "stop")
+                {
+                    Console.WriteLine("цикл остановлен");
+                    break;
+                }
+
+                switch (text)
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is red!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    case "yellow":
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Your color is yellow!");
+                        break;
+                    default:
+                        continue;
+
+                }
+                k++;
+            }
+
+            //Console.WriteLine("Цикл do while");
+            //int t = 1;
+            //do
+            //{
+            //    Console.WriteLine(t);
+            //    // тут switch код
+            //    t++;
+            //} while (t < 0);
+
+            Console.WriteLine("Введите имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Имя по буквам:");
+            foreach (var item in name)
+            {
+                Console.Write(" " + item);
+            }
+            Console.Write(" Последняя буква вашего имени: {0}", name[name.Length - 1]);
+
+
+
+        }
+    }
+}
