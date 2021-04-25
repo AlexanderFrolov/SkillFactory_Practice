5991f7eb2995838a8c61edc2f0713396272012f5
AlexanderFrolov <sovereignty.alex@gmail.com>
2021-04-25 11:41:31 +0300

    lesson_4_4_5 кортежи

---
+++ SkillFactory_Practice/Lesson_4_4_5_korteg/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lesson_4_4_5_korteg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string Type, double Age, int NameCount) Pet;
+
+            Console.WriteLine("Введите имя питомца");
+            Pet.Name = Console.ReadLine();
+            Pet.NameCount = Pet.Name.Length;
+
+            Console.WriteLine("Введите вид питомца");
+            Pet.Type = Console.ReadLine();
+
+            Console.WriteLine("Введите возрас питомца");
+            Pet.Age = double.Parse(Console.ReadLine());
+        }
+    }
+    }
+}
