a851599586f63f9228b2e2c815eee30705a9e4ad
AlexanderFrolov <sovereignty.alex@gmail.com>
2021-05-02 12:49:14 +0300

    работа с out ref и in

---
+++ SF_Practice_module_5/module_5_methods/5.3.11__5.3.13_out_ref_in/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _5._3._11__5._3._13_out_ref_in
+{
+    class Program
+    {   
+        static int SumNumbers(ref int num1, in int num2, out int num3, int num4)
+        {
+            num1 = 5;
+            num3 = num1 + num2;
+            return num3;
+        }
+
+        static void Main(string[] args)
+        {
+            int num1 = 2;
+            int num2 = 2;
+            int num4 = 2;
+            int num3 = SumNumbers(ref num1, num2, out num3, num4);
+            
+         
+            Console.WriteLine("Результат: {0}", num4 * num3);
+        }
+    }
+}
---
+++ SF_Practice_module_5/module_5_methods/5.3_parameters/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _5._3_parameters
+{
+    class Program
+    {
+        static void GetName(ref string name)
+        {
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+        }
+
+        static int[] GetArrayFromConsole(ref int num)
+        {
+            num = 6;
+            var result = new int[num];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
+
+        static void BigDataOperation(in int[] arr)
+        {
+            arr[0] = 4;
+        }
+        static void Main(string[] args)
+        {
+            var someName = "Евгения";
+            GetName(ref someName);
+            Console.WriteLine(someName);
+
+            var arr = new int[] { 1, 2, 3 };    
+            BigDataOperation(arr);
+
+            Console.WriteLine(arr[0]);
+        }
+    }
+}
