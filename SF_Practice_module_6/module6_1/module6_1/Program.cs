607b43e37b9ae53734e5986b98f9effbc629174d
AlexanderFrolov <sovereignty.alex@gmail.com>
2021-05-08 12:34:23 +0300

    этот модуль (о инкапсуляии) понятен и знаком

---
+++ SF_Practice_module_6/module6_1/module6_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace module6_1
+{
+
+    class Bus
+    {
+        public int? Load;
+
+        public void PrintStatus()
+        {
+            if (Load.HasValue)
+            {
+                Console.WriteLine("В авбтобусе {0} пассажиров", Load.Value);
+            }
+            else
+            {
+                Console.WriteLine("Автобус пуст!");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
