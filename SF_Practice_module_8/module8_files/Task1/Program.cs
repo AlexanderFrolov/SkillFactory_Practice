dc6afe725f77c2f1260053eeda370829cd97073c
AlexanderFrolov <sovereignty.alex@gmail.com>
2021-06-05 20:23:04 +0300

    Task1

---
+++ SF_Practice_module_8/module8_files/Task1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+
+        static void Delete_TimeSpan30()
+        {
+            //"C:/Users/Alexander Frolov/Desktop/temp"
+            Console.WriteLine("Укажите путь к папке в которой будут удалены файлы и папки не используемые более 30 минут:");
+            string dirName = Console.ReadLine();
+            var dirInfo = new DirectoryInfo(dirName);
+            if (dirInfo.Exists) // Проверим, что директория существует
+            {
+                TimeSpan diff;
+                try
+                {
+
+
+                    // пройдем по файлам. если больше 30 то удаляем.
+                    foreach (FileInfo file in dirInfo.GetFiles())
+                    {
+                        diff = DateTime.Now - file.LastAccessTime;
+                        if (diff.Minutes > 30)
+                        {
+                            Console.WriteLine($"Файл {file.Name} удален");
+                            file.Delete();
+                        }
+                    }
+
+                    // пройдем по папкам. если папка не спользовалась больше 30 мин то удаляем ее и все ее содержимое
+                    foreach (DirectoryInfo dir in dirInfo.GetDirectories())
+                    {
+                        diff = DateTime.Now - dir.LastAccessTime;
+                        if (diff.Minutes > 30)
+                        {
+                            Console.WriteLine($"Директория {dir.Name} удалена");
+                            dir.Delete(true);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("По указанному пути папки с таким именем не существует!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Удалим все что не использовалось больше 30 минут");
+            Delete_TimeSpan30();
+
+        }
+
+
+
+    }
+}
