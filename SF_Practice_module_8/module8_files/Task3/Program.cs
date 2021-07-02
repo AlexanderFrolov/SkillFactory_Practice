e6860fce15ec6fb06f6e95c3044ee87946f118cd
AlexanderFrolov <sovereignty.alex@gmail.com>
2021-07-02 12:32:36 +0300

    Task4  задание выполнено

--- SF_Practice_module_8/module8_files/Task3/Program.cs
+++ SF_Practice_module_8/module8_files/Task3/Program.cs
@@ -1,16 +1,43 @@
 ﻿using System;
 using System.IO;
 
-namespace Task1
+namespace Task3
 {
     class Program
     {
+        static long GetDirectorySize(string startDirectory, long size = 0)
+        {
+            try
+            {
+                string[] searchDirectory = Directory.GetDirectories(startDirectory);
+                if (searchDirectory.Length > 0)
+                {
+                    foreach (var dir in searchDirectory)
+                    {
+                        Console.WriteLine($"Папка: {dir}");
+                        size += GetDirectorySize(dir + @"\", size);
+                    }
+                }
+                string[] filesss = Directory.GetFiles(startDirectory);
+                foreach (var file in filesss)
+                {
+                    Console.WriteLine($"  Файл: {file}");
+                    FileInfo info = new FileInfo(file);
+                    size += info.Length;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return size;
+        }
 
         static void Delete_TimeSpan30(string dirName)
         {
             var dirInfo = new DirectoryInfo(dirName);
-            if (dirInfo.Exists) // Проверим, что директория существует
-            {
+          
                 TimeSpan diff;
                 try
                 {
@@ -41,22 +68,28 @@ namespace Task1
                 {
                     Console.WriteLine(e.Message);
                 }
+        }
 
+        static void Main(string[] args)
+        {
+            //"C:/Users/Alexander Frolov/Desktop/temp";
+            Console.WriteLine("Удалим все что не использовалось больше 30 минут");
+            Console.WriteLine("Укажите путь к папке в которой будут удалены файлы и папки не используемые более 30 минут:");
+            string dirName = Console.ReadLine();
+            if (Directory.Exists(dirName))
+            {
+                long result = GetDirectorySize(dirName);
+                Console.WriteLine($"Размер директории ДО удаления составляет: {result} байт");
+                Console.WriteLine("Выполняется очистка...");
+                Delete_TimeSpan30(dirName);
+                result = GetDirectorySize(dirName);
+                Console.WriteLine($"Размер директории ПОСЛЕ удаления составляет: {result} байт");
             }
             else
             {
                 Console.WriteLine("По указанному пути папки с таким именем не существует!");
             }
-        }
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Удалим все что не использовалось больше 30 минут");
-            //"C:/Users/Alexander Frolov/Desktop/temp"
-            Console.WriteLine("Укажите путь к папке в которой будут удалены файлы и папки не используемые более 30 минут:");
-            string dirName = Console.ReadLine();
-            Delete_TimeSpan30(dirName);
         }
-
     }
 }
---
+++ SF_Practice_module_8/module8_files/Task4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FinalTask
+{
+
+    [Serializable] //   Атрибут сериализации
+    class Student
+    {
+        //     Простая модель класса 
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateofBirth { get; set; }
+
+        // Метод - конструктор
+        public Student(string name, string group,  DateTime date)
+        {
+            Name = name;
+            Group = group;
+            DateofBirth = date;
+        }
+    }
+
+    class Program
+    {
+  
+        static void Main(string[] args)
+        {
+            string fileName = "../../../Students.dat";
+            // берем путь к рабочему столу из массива аргументов приложения args и создаем папку Students 
+            string mainPath = args[0] + "/Students";
+            Directory.CreateDirectory(mainPath);
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            // десериализация
+            using (var fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                Student[] Students = (Student[])formatter.Deserialize(fs);
+                Console.WriteLine("Объект десериализован");
+               
+                foreach (var student in Students)
+                {
+                    Console.WriteLine($"Имя: {student.Name} --- Группа: {student.Group} --- Дата рождения: {student.DateofBirth}");
+                    
+                    // распределяем студентов по группам студентов по файлам группы
+                    using (StreamWriter sw = File.AppendText(mainPath + "/" + student.Group + ".txt"))  
+                    {
+                        sw.WriteLine($"Имя: {student.Name}, Дата Рождения: {student.DateofBirth} ");
+                    }
+                   
+                }
+            }
+        }
+    }
+    
+}
