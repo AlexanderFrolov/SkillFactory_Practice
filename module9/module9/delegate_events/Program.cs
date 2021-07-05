105b87bea8c9168df18cb4effae682db45a38cd4
AlexanderFrolov <sovereignty.alex@gmail.com>
2021-07-05 18:06:20 +0300

    Выполнено домашнее задание по модулю 9

---
+++ module9/module9/delegate_events/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace delegate_events
+{
+    class MyException : FormatException
+    {
+        public MyException(string msg) : base(msg) { }
+        
+    }
+
+    class Program
+    {
+        static List<string> surnames = new List<string> { "Иванов", "Фролов", "Соколов", "Смирнов", "Петров" };
+
+        static void Main(string[] args)
+        {
+
+            SortingLastNames sortLastNames = new SortingLastNames();
+            sortLastNames.SortSurnamesEvent += SelectSortingMethod;
+
+            while (true)
+            {
+                try
+                {
+                    sortLastNames.ReadNumber();
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+
+        }
+
+        static void SelectSortingMethod(int number)
+        {   
+            switch (number)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Сортируем фамилии по возрастанию");
+                        surnames.Sort();                  
+                        foreach (var surname in surnames)
+                        {
+                            Console.WriteLine(surname);
+                        }       
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Сортируем фамилии по убыванию");
+                        surnames.Sort();
+                        surnames.Reverse();
+                        foreach (var surname in surnames)
+                        {
+                            Console.WriteLine(surname);
+                        }
+                        break;
+                    }
+            }
+        }
+
+    }
+
+    class SortingLastNames
+    {
+        public delegate void SortSurnamesDelegate(int number);
+        public event SortSurnamesDelegate SortSurnamesEvent;
+        public void ReadNumber()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Введите значение 1 если сортировка по возрастанию (А-Я) или 2 если по убыванию (Я-А):");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number != 1 && number != 2) { throw new MyException("Ошибка! Необходимо ввести значение 1 если сортировка по возрастанию или 2 если по убыванию"); }
+
+            Sort(number);
+
+        }
+
+        protected virtual void Sort(int number)
+        {
+            SortSurnamesEvent?.Invoke(number);
+        }
+
+    }
+
+}
---
+++ module9/module9/module9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace module9
+{
+
+    class MyException : Exception
+    {
+        public MyException(string msg) : base(msg) { }
+    }
+
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+
+            Exception[] exceptions = { new MyException("мое исключение"),
+                                       new IndexOutOfRangeException(),
+                                       new DivideByZeroException(),
+                                       new ArgumentException(),
+                                       new NotSupportedException(),
+                                       new TimeoutException()
+            };
+
+            foreach (var exception in exceptions)
+            {
+                
+                try
+                {
+                    throw exception;
+                }
+
+                catch (System.DivideByZeroException ex) { Console.WriteLine(ex.Message); }
+                catch (System.ArgumentException ex) { Console.WriteLine(ex.Message); }
+                catch (System.NotSupportedException ex) { Console.WriteLine(ex.Message); }
+                catch (System.IndexOutOfRangeException ex) { Console.WriteLine(ex.Message); }
+                catch (System.TimeoutException ex) { Console.WriteLine(ex.Message); }
+                catch (MyException ex) { Console.WriteLine(ex.Message); }
+            }
+        }
+    }
+}
