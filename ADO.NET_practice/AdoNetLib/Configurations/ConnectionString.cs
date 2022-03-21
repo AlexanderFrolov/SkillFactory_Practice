9b88c032aac0574f7c552c04db02fe4c27a9d731
AlexanderFrolov <sovereignty.alex@gmail.com>
2022-03-21 19:38:08 +0300

    ado.net теория и немного практики

---
+++ ADO.NET_practice/AdoNetLib/Configurations/ConnectionString.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace AdoNetLib
+{
+    public static class ConnectionString
+    {
+        public static string MsSqlConnection => @"Server=.\SQLEXPRESS01;Database=testing;Encrypt=False;Trusted_Connection=True;";
+    }
+}
