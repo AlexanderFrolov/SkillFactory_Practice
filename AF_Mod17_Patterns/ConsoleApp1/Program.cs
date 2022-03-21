0827739dd095b6034ec457131f3a5f853e20b3b9
AlexanderFrolov <sovereignty.alex@gmail.com>
2022-03-21 11:22:42 +0300

    ..

---
+++ AF_Mod17_Patterns/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Class1 clk = new Class1();
+            Class2 clk2 = new Class2();
+            clk2.Bca = 12;
+        }
+    }
+    
+}
---
+++ AF_Mod17_Patterns/ConsoleApp2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using ConsoleApp1;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            A a = new A();
+            B b = new B();
+            C c = new C();
+
+            a.Display();    // метод класса A
+            b.Display();    // метод класса B
+            ((A)b).Display();   // метод класса B
+
+            c.Display();    // метод класса C
+            ((A)c).Display();   // метод класса B
+            ((B)c).Display();   // метод класса B
+            D pica = new D();
+
+        }
+
+        class A
+        {
+            public virtual void Display()
+            {
+                Console.WriteLine("Вызван метод класса A");
+            }
+        }
+        class B : A
+        {
+            public override void Display()
+            {
+                Console.WriteLine("Вызван метод класса B");
+            }
+        }
+        class C : B
+        {
+            public new void Display()
+            {
+                Console.WriteLine("Вызван метод класса C");
+            }
+        }
+
+        public class D
+        {
+            public readonly int Count = 5;
+            public const double Pi = 3.141592; // неявно считается статическим
+            public D()
+            {
+                Count = 1;
+            }
+        }
+
+    }
+}
